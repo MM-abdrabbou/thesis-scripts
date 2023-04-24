86696536109d5a52d7ad0339041c7c51d7e52ac1
MM-abdrabbou <121033323+MM-abdrabbou@users.noreply.github.com>
2023-04-24 12:00:55 +0200

    Update and rename bcftools_isec.sh to 50-BCFTtools_isec.sh

--- 50-BCFTtools_isec.sh
+++ 50-BCFTtools_isec.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# This script generates all the pairwise intresections among the 7 pipelines using bcftools isec
+
 IN_PATH1=/media/rna/INIA/benchmark/Concordance/bcftools_isec/PATH_1
 IN_PATH2=/media/rna/INIA/benchmark/Concordance/bcftools_isec/PATH_2
 Out=/media/rna/INIA/benchmark/Concordance/bcftools_isec/Output
@@ -13,4 +16,4 @@ for file1 in $IN_PATH1/*.gz; do
     done
 done 
 
-bcftools query -f '%CHROM\_%POS\n'  | head
+#bcftools query -f '%CHROM\_%POS\n'  | head
