1f00be6cc246c33bfe1c13f1699d10832a319084
MM-abdrabbou <121033323+MM-abdrabbou@users.noreply.github.com>
2023-04-24 11:42:24 +0200

    Update change_samples_names.sh

--- change_samples_names.sh
+++ change_samples_names.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#############################################################################
+#                                                                           #
+# Octopus and gatk has a problem in individual sample naming                #   
+# so this script was used to change sample names before merging vcf files   #
+#############################################################################
+
+
 #PATH_IN_VCF=/media/rna/INIA/benchmark/Variant_calling/BCF_Tools/bwa-mem/VCF_output
 PATH_OUT=/media/rna/INIA/benchmark/Variant_calling/BCF_Tools/bowtie_nnsamples
 mkdir -p $PATH_OUT
@@ -12,6 +19,7 @@ cd /media/rna/INIA/benchmark/Variant_calling/BCF_Tools/Bowtie2/VCF_output
 #for f in *.gz ; do tabix -p vcf $f ; done
 #for entry in *.gz; do echo $entry; done
 
+#loop over the 100 samples to change their names in the vcf files
 for folder in *.vcf ; do
     sample=$(basename $folder .vcf)
     echo $sample >> $file
