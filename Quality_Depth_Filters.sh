08439d85b3a4bb455c009349eae5dbd4fc2b383c
MM-abdrabbou <121033323+MM-abdrabbou@users.noreply.github.com>
2023-04-24 10:29:37 +0200

    Update Quality_Depth_Filters.sh

--- Quality_Depth_Filters.sh
+++ Quality_Depth_Filters.sh
@@ -13,6 +13,6 @@ PATH_OUT=' '
 file= ' '
 
 
-bcftools view -e 'QUAL <= 30' /$PATH_IN/$file   -o /PATH_IN/$output
+bcftools view -e 'QUAL <= 30' /$PATH_IN/$file   -o /PATH_OUT/output_qual.vcf
 
-bcftools view -e 'INFO/DP <= 60' /$PATH_IN/$file   -o /PATH_IN/$output
+bcftools view -e 'INFO/DP <= 60' /$PATH_IN/output_qual.vcf  -o /PATH_OUT/output_qual_dp.vcf
